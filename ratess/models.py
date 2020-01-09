9cd7be275d956c63965fb4183acbcc101da04524
bellahOchola <bellahkenya@gmail.com>
2020-01-09 14:00:07 +0300

    create project model that showcases project info

--- ratess/models.py
+++ ratess/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length = 100)
+    description = models.TextField(max_length = 500)
+    image_pic = models.ImageField(upload_to = 'images/')
+    posted = DateTimeField()
