a6b40e90cd5e5560521cda249d8bba49de487cec
bellahOchola <bellahkenya@gmail.com>
2020-01-09 14:17:04 +0300

    create a view for the project api

--- ratess/views.py
+++ ratess/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from .models import Project
+from .serializer import ProjectSerializer
 
+#........
 # Create your views here.
+class ProjectList(APIView):
+    def get(self, request, format=None):
+        all_projects = Project.objects.all()
+        serializers = ProjectSerializer(all_projects, many=True)
+        return Response(serializers.data)
+
