9e73f45f1bfcbdd22fefdf68d79c5c50ebdc8d28
bellahOchola <bellahkenya@gmail.com>
2020-01-09 16:30:16 +0300

    add post request function that allows posting of data to the database

--- ratess/views.py
+++ ratess/views.py
@@ -3,12 +3,23 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Project
 from .serializer import ProjectSerializer
+from rest_framework import status   # handles all status code responses
 
 #........
 # Create your views here.
 class ProjectList(APIView):
+
+    # handling a retrieval request
     def get(self, request, format=None):
         all_projects = Project.objects.all()
         serializers = ProjectSerializer(all_projects, many=True)
         return Response(serializers.data)
+
+    # handling a post request
+    def post(self,request, format=None):
+        serializers = ProjectSerializer(data=request.data)
+        if serializers.is.valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status = status.HTTP_400_BAD_REQUEST)
 
