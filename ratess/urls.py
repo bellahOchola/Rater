ef4e5d700db69212ba8d6726b329ba10337010ae
bellahOchola <bellahkenya@gmail.com>
2020-01-10 11:37:59 +0300

    add functionality that creates the profile api

--- ratess/urls.py
+++ ratess/urls.py
@@ -4,5 +4,6 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('api/project/', views.ProjectList.as_view()),
+    path('api/profile/', views.ProfileList.as_view()),
     path('api-token-auth/', obtain_auth_token)
 ]
--- ratess/views.py
+++ ratess/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from .models import Project
-from .serializer import ProjectSerializer
+from .models import Project, Profile
+from .serializer import ProjectSerializer, ProfileSerializer
 from rest_framework import status   # handles all status code responses
 
 #........
@@ -23,3 +23,17 @@ class ProjectList(APIView):
             return Response(serializers.data, status=status.HTTP_201_CREATED)
         return Response(serializers.errors, status = status.HTTP_400_BAD_REQUEST)
 
+class ProfileList(APIView):
+
+    # handling a retrieval request
+    def get(self, request, format=None):
+        all_profiles = Profile.objects.all()
+        serializers = ProfileSerializer(all_profiles, many=True)
+        return Response(serializers.data)
+
+    def post(self, request, format=None):
+        serializers = ProfileSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status = status.HTTP_400_BAD_REQUEST)
