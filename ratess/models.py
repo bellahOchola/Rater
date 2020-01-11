e6de71e3d59eddffac5d66ff156c74830423a7b0
bellahOchola <bellahkenya@gmail.com>
2020-01-11 09:09:08 +0300

    add funtions to the models that delete and save their content

--- ratess/models.py
+++ ratess/models.py
@@ -7,9 +7,38 @@ class Project(models.Model):
     image = ImageField(blank=True, manual_crop="")
     description = models.TextField(max_length = 500)
     posted = models.DateTimeField(auto_now_add=True, null=True)
-    
+
+    def __str__(self):
+        return f'{self.user.name} Caption'
+
+    def save_project(self):
+        self.save()
+
+    def delete_project(self):
+        self.delete()
+
 
 class Profile(models.Model):
     bio = models.TextField(max_length = 500)
     profile_pic = ImageField(blank=True, manual_crop="")
     location = models.CharField(max_length = 100) 
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
+
+    def save_profile(self):
+        self.user
+
+    def delete_profile(self):
+        self.delete()
+    
