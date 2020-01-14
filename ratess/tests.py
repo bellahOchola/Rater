e3b515b74883d2eb0db117c61cce7070882e792b
bellahOchola <bellahkenya@gmail.com>
2020-01-14 16:20:13 +0300

    deployment

--- ratess/tests.py
+++ ratess/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Project, Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class TestProject(TestCase):
+    def setUp(self):
+        
